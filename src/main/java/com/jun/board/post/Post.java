a29ead09a1219f4820f959f9fb3971faa8f384ed
changjun6518 <aowert6518@gmail.com>
2022-10-06 21:43:54 +0900

    #add: project init

---
+++ src/main/java/com/jun/board/post/Post.java
@@ -0,0 +1,18 @@
+package com.jun.board.post;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+
+}
