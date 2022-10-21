f07e6fa2ae7a629f5f43ddcc6b607864632ec658
changjun6518 <aowert6518@gmail.com>
2022-10-21 21:24:47 +0900

    #add: 게시글을 볼수있다

--- src/main/java/com/jun/board/post/PostApi.java
+++ src/main/java/com/jun/board/post/PostApi.java
@@ -1,46 +1,20 @@
 package com.jun.board.post;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/post")
+@RequestMapping("/post")
 public class PostApi {
 
     private final PostService postService;
 
-    @GetMapping("")
-    public ResponseEntity<List<PostReadDto>> getPosts() {
-        List<PostReadDto> postReadDtoList = postService.findAll();
-        return new ResponseEntity<>(postReadDtoList, HttpStatus.OK);
-    }
-
     @PostMapping("")
-    public ResponseEntity<Void> updatePost(@RequestBody PostCreateDto postCreateDto) {
+    public void createPost(@RequestBody PostCreateDto postCreateDto) {
         postService.create(postCreateDto);
-        return ResponseEntity.ok().build();
-    }
-
-    @PutMapping("")
-    public ResponseEntity<Void> updatePost(@RequestBody PostUpdateDto postUpdateDto) {
-        postService.update(postUpdateDto);
-        return ResponseEntity.ok().build();
-    }
-
-    @DeleteMapping("")
-    public ResponseEntity<Void> deletePost(@RequestParam Long postId) {
-        postService.delete(postId);
-        return ResponseEntity.ok().build();
     }
 }
